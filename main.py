f61adac2ebd706f8acd55147e236cf83ee669b07
unknownpgr <unknownpgr@gmail.com>
2023-04-17 14:54:27 +0000

    Minor fix

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ def turn_on_ec2():
     '''
     Turn on EC2 instance
     '''
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.resource('ec2',
+                         aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
+                         region_name=config.AWS_REGION
+                         )
     ec2.instances.filter(InstanceIds=config.EC2_INSTANCE_IDS).start()
 
 
@@ -27,21 +31,26 @@ def turn_off_ec2():
     '''
     Turn off EC2 instance
     '''
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.resource('ec2',
+                         aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
+                         region_name=config.AWS_REGION
+                         )
     ec2.instances.filter(InstanceIds=config.EC2_INSTANCE_IDS).stop()
 
 
 print("Start EC2 scheduler")
 
 while True:
-    # Turn on EC2 at 04:55 PM
-    # Turn off EC2 at 00:10 AM
+    # Turn on EC2 at 16:55
+    # Turn off EC2 at 00:10
 
     now = get_time()
-    if now.hour == 11 and now.minute == 45:
+    print(f"{now} - {now.hour}:{now.minute}")
+    if now.hour == 15 and now.minute == 55:
         print(f"{now} - Turn on EC2")
         turn_on_ec2()
-    elif now.hour == 11 and now.minute == 43:
+    elif now.hour == 0 and now.minute == 10:
         print(f"{now} - Turn off EC2")
         for i in range(5):
             print(f"{now} - Count {i}")
