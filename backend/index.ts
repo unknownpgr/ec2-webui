71cbf14e0cb94b72e9f98e949d78caac97213c17
unknownpgr <unknownpgr@gmail.com>
2023-04-24 09:15:43 +0000

    Implement simple ui

--- backend/index.ts
+++ backend/index.ts
@@ -6,7 +6,8 @@ import {
   StopInstancesCommand,
   DescribeInstancesCommand,
 } from "@aws-sdk/client-ec2";
-import session from "express-session";
+import fs from "fs/promises";
+import crypto from "crypto";
 
 declare module "express-session" {
   interface SessionData {
@@ -29,7 +30,22 @@ interface Ec2State {
   };
 }
 
+interface Ec2Schedule {
+  instanceId: string;
+  startupTime: string;
+  shutdownTime: string;
+}
+
+interface Config {
+  sessions: string[];
+  ec2Schedules: Ec2Schedule[];
+}
+
 const ec2State: Ec2State[] = [];
+const config: Config = {
+  sessions: [],
+  ec2Schedules: [],
+};
 
 const ec2Client = new EC2Client({
   region: process.env.AWS_REGION,
@@ -85,52 +101,86 @@ async function stopEc2(instanceId: string) {
   await ec2Client.send(command);
 }
 
+async function getSession() {
+  const session = crypto.randomBytes(16).toString("hex");
+  config.sessions.push(session);
+  await saveConfig();
+  return session;
+}
+
+async function loadConfig() {
+  const data = await fs.readFile("config.json", "utf-8");
+  try {
+    const json = JSON.parse(data);
+    config.sessions = json.sessions || [];
+    config.ec2Schedules = json.ec2Schedules || [];
+  } catch (e) {}
+}
+
+async function saveConfig() {
+  const json = JSON.stringify(config);
+  await fs.writeFile("config.json", json);
+}
+
 async function main() {
   await updateEc2State();
+  await loadConfig();
 
   const app = express();
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET || "EXPRESS_SESSION_SECRET",
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
 
   app.use(express.static("public"));
   app.use(express.json());
 
-  app.post("/login", (req, res) => {
+  app.post("/api/login", async (req, res) => {
     if (req.body.password === WEBUI_PASSWORD) {
-      req.session.isLoggedIn = true;
+      const session = await getSession();
       res.json({
         success: true,
+        session,
       });
     } else {
-      req.session.destroy(() => {
-        res.json({
-          success: false,
-        });
-      });
-    }
-  });
-
-  app.use((req, res, next) => {
-    if (req.session.isLoggedIn) {
-      next();
-    } else {
-      res.send({
+      res.status(401).json({
         success: false,
       });
     }
   });
 
-  app.get("/ec2", async (req, res) => {
+  app.use((req, res, next) => {
+    const session = (req.headers.session as string) || "";
+    if (config.sessions.includes(session)) {
+      next();
+    } else {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+  });
+
+  app.get("/api/test", async (req, res) => {
+    res.json({
+      success: true,
+    });
+  });
+
+  app.get("/api/logout", async (req, res) => {
+    const session = (req.headers.session as string) || "";
+    const index = config.sessions.indexOf(session);
+    if (index >= 0) {
+      config.sessions.splice(index, 1);
+      await saveConfig();
+    }
+    res.json({
+      success: true,
+    });
+  });
+
+  app.get("/api/ec2", async (req, res) => {
     await updateEc2State();
     res.json(ec2State);
   });
 
-  app.post("/ec2/start", async (req, res) => {
+  app.post("/api/ec2/start", async (req, res) => {
     const instanceId = req.body.instanceId;
     await startEc2(instanceId);
     res.json({
@@ -138,7 +188,7 @@ async function main() {
     });
   });
 
-  app.post("/ec2/stop", async (req, res) => {
+  app.post("/api/ec2/stop", async (req, res) => {
     const instanceId = req.body.instanceId;
     await stopEc2(instanceId);
     res.json({
@@ -146,8 +196,8 @@ async function main() {
     });
   });
 
-  app.listen(80, () => {
-    console.log("Listening on port 80");
+  app.listen(8080, () => {
+    console.log("Listening on port 8080");
   });
 }
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,133 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface Ec2State {
+  instanceType: string;
+  instanceName: string;
+  instanceId: string;
+  publicIpAddress: string;
+  state: {
+    code: number;
+    name: string;
+  };
+}
+
+async function get(path: string, session: string) {
+  const response = await fetch(path, {
+    headers: {
+      session,
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+async function post(path: string, session: string, body: any) {
+  const response = await fetch(path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      session,
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  return data;
+}
 
 function App() {
-  return <div></div>;
+  const [password, setPassword] = useState("");
+  const [session, setSession] = useState("");
+  const [ec2, setEc2] = useState<Ec2State[]>([]);
+  const [lastUpdate, setLastUpdate] = useState(0);
+  const isLoggedIn = session !== "";
+
+  async function onLogin() {
+    const res = await post("/api/login", "", { password });
+    if (res.success) {
+      localStorage.setItem("session", res.session);
+      setSession(res.session);
+    }
+  }
+
+  async function onLogout() {
+    const res = await post("/api/logout", session, {});
+    if (res.success) {
+      localStorage.removeItem("session");
+      setSession("");
+    }
+  }
+
+  async function updateEc2() {
+    const res = (await get("/api/ec2", session)) as Ec2State[];
+    setEc2(res);
+    setLastUpdate(Date.now());
+  }
+
+  useEffect(() => {
+    (async () => {
+      const session = localStorage.getItem("session");
+      if (!session) return;
+      const res = await get("/api/test", session);
+      if (res.success) {
+        setSession(session);
+      } else {
+        localStorage.removeItem("session");
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+    updateEc2();
+  }, [isLoggedIn, session]);
+
+  return (
+    <div>
+      <h1>EC2 Controller</h1>
+      {!isLoggedIn ? (
+        <div>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={onLogin}>Login</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={onLogout}>Logout</button>
+        </div>
+      )}
+      <button onClick={updateEc2}>Update</button>
+      <p>Last update: {new Date(lastUpdate).toLocaleString()}</p>
+      {ec2.map((e) => (
+        <div key={e.instanceId}>
+          <h2>{e.instanceName}</h2>
+          <p>{e.instanceType}</p>
+          <p>{e.publicIpAddress}</p>
+          <p>{e.state.name}</p>
+
+          <button
+            onClick={async () => {
+              await post("/api/ec2/start", session, {
+                instanceId: e.instanceId,
+              });
+            }}
+          >
+            Start
+          </button>
+          <button
+            onClick={async () => {
+              await post("/api/ec2/stop", session, {
+                instanceId: e.instanceId,
+              });
+            }}
+          >
+            Stop
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
