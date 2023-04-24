043ac3ccb1d841d1ea86cdb8c176a93f3f2c76db
unknownpgr <unknownpgr@gmail.com>
2023-04-24 10:01:23 +0000

    Remove legacy

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.9
-
-RUN pip install boto3
-
-WORKDIR /app
-
-COPY main.py .
-
-CMD ["python", "main.py"]
--- run.sh
+++
@@ -1,6 +0,0 @@
-docker build -t ec2-runner .
-docker run\
-    --rm\
-    -it\
-    -v `pwd`/config.py:/app/config.py\
-    ec2-runner
